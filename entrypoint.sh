82f079b254e8701aaf4e4b1a1f640e8d34da2511
Gilles Robin <gilles.robin@uca.fr>
2025-01-24 10:53:36 +0100

    Downgrade PHP version to 8.3 in Dockerfile and update entrypoint script to start all supervisord processes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,11 @@
 printenv > /etc/environment
 
 # Start supercronic using supervisord
-supervisord -c /etc/supervisor/conf.d/supercronic.conf && sleep 2 && supervisorctl start supercronic
+supervisord -c /etc/supervisor/conf.d/supercronic.conf && supervisorctl start all
+
+if [ -f /tmp/entrypoint.sh ]; then
+    . /tmp/entrypoint.sh
+fi
 
 # Start FrankenPHP
 frankenphp run caddy --config /etc/caddy/Caddyfile --adapter caddyfile
